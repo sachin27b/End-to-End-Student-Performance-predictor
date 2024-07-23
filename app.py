993118029570e70424a087920b364cbb6e542681
sachin <sachin270895@gmail.com>
2024-07-23 16:24:39 +0530

    done

--- app.py
+++ app.py
@@ -2,20 +2,16 @@ from flask import Flask, request,render_template
 import numpy as np
 import pandas as pd
 
-from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData
 from src.pipeline.predict_pipeline import PredictPipeline
 application=Flask(__name__)
 app=application
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 @app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method=='GET':
-        return render_template('home.html')
+        return render_template('index.html')
     else:
         data=CustomData(
             gender=request.form.get('gender'),
@@ -33,7 +29,7 @@ def predict_datapoint():
 
         predict_pipeline=PredictPipeline()
         results=predict_pipeline.predict(pred_df)
-        return render_template('home.html', results=results[0])
+        return render_template('index.html', results=results[0])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
